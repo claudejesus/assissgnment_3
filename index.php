4a46ea92a6b41706434b327e5c0bdea9db280d9a
claudejesus <niyigabaclaujesus.com>
2025-01-30 11:31:19 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div>
+    <h1>Welcome to index page of our system </h1>
+
+    <a href="registration.php">Register Here</a> | <a href="login.php">Login page</a>
+
+
+</div>
+</body>
+</html>
